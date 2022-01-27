700fd57051284c7c8dbb8e4795283319d2e42459
Preterra Corp <victor@preterra.com>
2022-01-27 15:56:41 -0700

    styling

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -1,8 +1,12 @@
-<div>
+<div class="w-full">
     <button onclick="window.location='{{ url("insurance/add") }}'">New Policy Wizard</button>
-    @forelse($policies as $policy)
-        <div><a href="{{url('/insurance', $policy)}}"> {{$policy->brokerage}}</a</div>
-    @empty
-        No Policies found.
+    <div class="w-full pl-5 pt-3">
+        @forelse($policies as $policy)
+            <div class="cursor-pointer mb-2 hover:bg-green-100">
+                <a class="w-full" href="{{url('/insurance', $policy)}}"> {{$policy->brokerage}}</a>
+                </div>
+        @empty
+            No Policies found.
         @endforelse
+    </div>
 </div>
